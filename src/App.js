c1f5e25a14ccb4e8ebcb2ab3aa322d5d6d2258de
Juhyun <julia.juhyun@gmail.com>
2022-12-31 12:39:53 +0900

    1230

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainPageContainer from "./pages/main/MainPageContainer";
-import "./index.scss";
 
 function App() {
   return (
--- src/pages/main/MainPageContainer.js
+++ src/pages/main/MainPageContainer.js
@@ -1,9 +1,33 @@
-import react from "react";
+import react, { useEffect } from "react";
+import "../../styles/index.css";
+import Observer from "../../utils/observer";
 
 const MainPageContainer = () => {
+  // []안에 있는 값이 바뀔 때만 실행되는게 useEffect.
+  useEffect(() => {
+    Observer();
+  }, []);
+
   return (
-    <div>
-      <h1>Home Page</h1>
+    <div className="container">
+      <div className="hidden">
+        <div className="center">
+          <span>Autrism Support Network</span>
+        </div>
+      </div>
+      <div className="hidden vertical">
+        <div>
+          <img src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumb2.gettyimageskorea.com%2Fimage_preview%2F700%2F201709%2FEYM%2F752194037.jpg&type=sc960_832" />
+          <p>설명이 들어갈 자리입니다.</p>
+        </div>
+        <div>
+          <p>설명이 들어갈 자리입니다.</p>
+          <img src="https://search.pstatic.net/sunny/?src=https%3A%2F%2Fthumb2.gettyimageskorea.com%2Fimage_preview%2F700%2F201709%2FEYM%2F752194037.jpg&type=sc960_832" />
+        </div>
+      </div>
+      <div className="hidden">
+        <h1>Home Page</h1>
+      </div>
     </div>
   );
 };
