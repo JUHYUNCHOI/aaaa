de5e4d91a841c3ff764de5ca27889c9b71f0e1e2
Juhyun <julia.juhyun@gmail.com>
2023-02-12 10:29:00 +0900

    0205

--- src/common/Map.js
+++ src/common/Map.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 export default function Map() {
-  // return <div></div>
   return (
     <iframe
       width="600"
@@ -10,7 +9,7 @@ export default function Map() {
       loading="lazy"
       allowFullscreen
       referrerPolicy="no-referrer-when-downgrade"
-      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBTdA_CZDkBE3mEJKA7f_9BB9k092w390Q&q=Vietnam+hospital+police">
+      src="https://www.google.com/maps/embed/v1/place?key=AIzaSyBTdA_CZDkBE3mEJKA7f_9BB9k092w390Q&q=hochiminh+hospital/@10.7822021,106.6428999,13z/data=!3m1!4b">
     </iframe>
     // <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfcoleH1fpMe3rOhKazZPWr0VICSiaZG--deXohnG1SrjF0bw/viewform?embedded=true" width="640" height="382" frameborder="0" marginheight="0" marginwidth="0">로드 중…</iframe>
   )
---
+++ src/pages/page3/Page3.js
@@ -0,0 +1,7 @@
+import react from "react";
+
+const Page3 = () => {
+  return (<div></div>);
+};
+
+export default Page3;
